470cf7dbea5815ba6bf2245cfaa2ab09b6e58d8c
Sneha Begari <begarii.sneha@gmail.com>
2026-07-08 16:55:33 +0530

    Refactor menu toggle functionality in script.js

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const hamburger=document.querySelector(".hamburger")
+/*const hamburger=document.querySelector(".hamburger")
     const menu_bar =document.querySelector(".navbar-menu .menu-bar ")
     hamburger.addEventListener("click",()=>{ 
         menu_bar.classList.toggle("show")
+});
+
+document.querySelectorAll(".navbar-menu .menu-bar a").forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("show");
+    });
+});
+
+
+const links = document.querySelectorAll(".navbar-menu .menu-bar a");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("show");
+    });
+});*/
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".navbar-menu .menu-bar");
+const menuLinks = document.querySelectorAll(".navbar-menu .menu-bar a");
+
+// Open/Close menu
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
+
+// Close menu when a link is clicked
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("show");
+    });
 });
